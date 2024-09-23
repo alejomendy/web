8f7b131bc4b71672b6121e206a6a647094c7d2c2
alejomendy <alejomendi24@gmail.com>
2024-09-23 11:56:29 -0300

    update page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4137074179"
+    serviceWorkerVersion: "227888036"
   }
 });
